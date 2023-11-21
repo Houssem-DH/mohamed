6c37e78f7c59ee38def8eab751db87a291974eb0
Houssem Eddine Dahel <houssemoodahel@gmail.com>
2023-11-21 19:13:14 +0100

    FIX

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <AboutSection/>
-      <br /><br />
+      <br /><br /><br /><br />
       <Project/>
      
     </main>
